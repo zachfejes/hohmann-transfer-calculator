2f602061c34e49958fbb34efa7eadbe07f161ed2
Zach <zacharyfejes@gmail.com>
2018-01-28 16:31:35 -0500

    Created method for calculating transit time along the ellipse connecting two circular orbits (hohmann transfer). #2

--- src/js/scripts/hohmannTransitTime.js
+++ src/js/scripts/hohmannTransitTime.js
@@ -1,38 +1,55 @@
 import { G } from './constants';
 
 /**
- * @description Calculates the orbital speed of an object (trivial mass), orbiting a significantly larger object.
- * @param {number} r_i Radius of the initial circular orbit, and pariapsis of the transfer ellipse.
- * @param {number} r_f Radius of the goal circular orbit, and apoapsis of the transfer ellipse.
- * @param {number} M Mass of the non-trivial body at the foci of the transfer ellipse.
- * @returns {number} Returns the TOF across the transfer ellipse from the initial to final orbit.
+ * @description Calculates the TOF across the transfer ellipse from the initial to the final orbit.
+ * @param {number} r_i Radius of the initial circular orbit, and pariapsis of the transfer ellipse [m].
+ * @param {number} r_f Radius of the goal circular orbit, and apoapsis of the transfer ellipse [m].
+ * @param {number} M Mass of the non-trivial body at the foci of the transfer ellipse [kg].
+ * @returns {number} Returns the TOF across the transfer ellipse from the initial to final orbit [s].
  */
 function hohmannTransitTime(r_i, r_f, M) {
-    if (v === undefined) {
+    if (r_i === undefined) {
       throw new Error(
-        "v parameter must be defined, but value passed is undefined."
+        "r_i parameter must be defined, but value passed is undefined."
       );
     }
-    if (e === undefined) {
+    if (r_f === undefined) {
       throw new Error(
-        "e parameter must be defined, but value passed is undefined."
+        "r_f parameter must be defined, but value passed is undefined."
+      );
+    }
+    if (M === undefined) {
+      throw new Error(
+        "M parameter must be defined, but value passed is undefined."
       );
     }
   
-    if (isNaN(v)) {
-      throw new Error("v parameter must be a number, but value passed is NaN.");
+    if (isNaN(r_i)) {
+      throw new Error("r_i parameter must be a number, but value passed is NaN.");
     }
-  
-    if (isNaN(e)) {
-      throw new Error("e parameter must be a number, but value passed is NaN.");
+    if (isNaN(r_f)) {
+      throw new Error("r_f parameter must be a number, but value passed is NaN.");
+    }
+    if (isNaN(M)) {
+      throw new Error("M parameter must be a number, but value passed is NaN.");
+    }
+
+    if (r_i <= 0) {
+      throw new Error("r_i parameter must be a non-zero positive number.");
+    }
+    if (r_f <= 0) {
+      throw new Error("r_f parameter must be a non-zero positive number.");
+    }
+    if (M <= 0) {
+      throw new Error("M parameter must be a non-zero positive number.");
     }
     
-    var fpa;
+    var transitTime;
   
-    fpa = Math.atan2(e*Math.sin(v),(1 + e*Math.cos(v)));
+    transitTime = Math.PI*Math.sqrt(Math.pow((r_i + r_f),3)/(8*G*M));
   
-    return fpa;
+    return transitTime;
   }
   
-  export default flightPathAngle;
+  export default hohmannTransitTime;
   
